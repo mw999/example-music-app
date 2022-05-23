bdbbcd1a607ad7db7be3c57caea6b36eb4e32ae4
Matthew Williams <mw999@hotmail.co.uk>
2022-05-23 19:41:49 +0100

    revert knex

--- packages/api/src/db/knexfile.ts
+++ packages/api/src/db/knexfile.ts
@@ -2,7 +2,7 @@ import type { Knex } from "knex";
 
 require("dotenv").config({ path: "../../.env" });
 
-const connection: Knex.StaticConnectionConfig = {
+const standard: Knex.StaticConnectionConfig = {
   timezone: "+00:00",
   charset: "utf8mb4",
   port: 3306,
@@ -10,6 +10,16 @@ const connection: Knex.StaticConnectionConfig = {
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+};
+
+const testing: Knex.StaticConnectionConfig = {
+  timezone: "+00:00",
+  charset: "utf8mb4",
+  port: parseInt(process.env.TEST_DATABASE_PORT),
+  host: process.env.TEST_DATABASE_HOST,
+  user: process.env.TEST_DATABASE_USER,
+  password: process.env.TEST_DATABASE_PASSWORD,
+  database: process.env.TEST_DATABASE_NAME || "test_database",
 };
 
 const knexfile: Knex.Config = {
@@ -20,7 +30,7 @@ const knexfile: Knex.Config = {
   seeds: {
     directory: `${__dirname}/seeds`,
   },
-  connection
+  connection: process.env.NODE_ENV === "test" ? testing : standard,
 };
 
 export default knexfile;
