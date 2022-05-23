505558660eb5691b791b9c3cf3cc90577f5d42bb
Matthew Williams <mw999@hotmail.co.uk>
2022-05-23 19:07:07 +0100

    fix ci

--- packages/api/jest/jest.migrate.js
+++ packages/api/jest/jest.migrate.js
@@ -5,7 +5,15 @@ require("dotenv").config();
 const knexfile = require("../src/db/knexfile");
 
 const migrateDatabase = async () => {
-  const knex = Knex(knexfile);
+  const knex = Knex({
+    timezone: "+00:00",
+    charset: "utf8mb4",
+    port: parseInt(process.env.TEST_DATABASE_PORT),
+    host: process.env.TEST_DATABASE_HOST,
+    user: process.env.TEST_DATABASE_USER,
+    password: process.env.TEST_DATABASE_PASSWORD,
+    database: process.env.TEST_DATABASE_NAME || "test_database",
+  });
   await knex.migrate.latest();
   return knex.destroy();
 };
